216b0e429a625795606647a35a228c9e31ff1f7e
Tiago Goulart <tiagolandim_@hotmail.com>
2023-07-10 21:17:23 -0300

    Update dissolve_buffer.py

--- dissolve_buffer.py
+++ dissolve_buffer.py
@@ -1,27 +1,27 @@
-# Importe as bibliotecas necessárias
+# Import the necessary libraries
 import geopandas as gpd
 import numpy as np
 import shapely;
 import matplotlib.pyplot as plt
 
-# Leia o shapefile com os dados 
-ufs=gpd.read_file(r'C:\Users\tiago\Área de Trabalho\PROJECTS\MAPA DENSIDADE BRASIL\BR_UF_2022\BR_UF_2022.shp')
+# Read the shapefile with the data
+ufs=gpd.read_file(r'C:\Users\BR_UF_2022.shp')
 
-# Criei uma coluna para utilizar de atributo para dissolver as geometrias dos estados e
-# criar uma geometria única para o país
+# Create a column to use as an attribute to dissolve the geometries of the states and
+# create a unique geometry for the country
 ufs['pais']=1
 
-# Dissolvendo as geometrias
+# Dissolving the geometries
 brasil=ufs.dissolve(by='pais')
 
 from matplotlib.patches import Patch
 
-# Reprojeta a geometria
+# Reproject the geometry
 brasil_mercator= brasil.to_crs('EPSG:5641')
-# Gera os buffers  de acordo com os atributos especificados pelos argumentos
+# Generate the buffers according to the attributes specified by the arguments
 buffer= brasil_mercator.buffer(distance=100000, resolution=10,cap_style=1, join_style=1, mitre_limit=5.0, single_sided=False)
 
-# Visualizando o resultado
+# Viewing the result
 f, ax = plt.subplots()
 buffer.plot(ax=ax, color='green', label='Brasil')
 brasil_mercator.plot(ax=ax, color='yellow', label='Buffer')
